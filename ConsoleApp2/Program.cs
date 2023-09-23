4aef9f3bb0fee4eeeb2c3cea0c07221d106f1dd4
student <student@201-53>
2023-09-23 11:17:33 +0800

    加入專案檔案。

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius;
+            Console.Write("請輸入圓的半徑： ");
+
+            if (double.TryParse(Console.ReadLine(), out radius))
+            {
+                double surfaceArea = Math.PI * Math.Pow(radius, 2);
+                Console.WriteLine("圓的面積是： " + surfaceArea);
+            }
+            else
+            {
+                Console.WriteLine("無效的輸入。請輸入一個有效的數字。");
+            }
+        }
+    }
+}
